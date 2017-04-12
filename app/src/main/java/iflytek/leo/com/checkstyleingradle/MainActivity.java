23942ff6196c3fdfca106e0688d31437ed1139e6
jinchao <dms.jinchao@gmail.com>
2017-04-12 15:12:06 +0800

    测试

--- app/src/main/java/iflytek/leo/com/checkstyleingradle/MainActivity.java
+++ app/src/main/java/iflytek/leo/com/checkstyleingradle/MainActivity.java
@@ -1,7 +1,7 @@
 package iflytek.leo.com.checkstyleingradle;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +9,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+private void a(){
+
+}
+private int b()     {
+return 2;
+}
+
 }
